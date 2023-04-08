8a1cc4864804b1ce7c8e8fcd79877fd93a8d2b91
Santiago Hincapie <santiago.augusto48@gmail.com>
2023-04-08 16:27:49 -0500

    Modificacion ramas

--- Back/src/main/java/com/example/Back/BackApplication.java
+++ Back/src/main/java/com/example/Back/BackApplication.java
@@ -1,13 +1,13 @@
-package com.StockVision.stockVision;
+package com.example.Back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class BackApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(BackApplication.class, args);
 	}
 
 }
